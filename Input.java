63aa0c276ca02988f858ba5591e7bb8ca96b35f4
craviee <danielcraviee@gmail.com>
2020-01-23 16:11:45 +0100

    Renaming and Implementing ReadData.java

---
+++ Input.java
@@ -0,0 +1,23 @@
+public class Input
+{
+    String contractType;
+    double income;
+
+    public Input()
+    {
+        try {
+            System.out.print("Enter income: ");
+            income = Double.parseDouble(br.readLine());
+            System.out.print("Contract Type: (E)mployment, (C)ivil: ");
+            contractType = br.readLine().charAt(0);
+        
+        } catch (Exception ex) {
+            System.out.println("Incorrect");
+            System.err.println(ex);
+            return;
+        }
+    }
+
+    public double getIncome() { return Income; }
+    public String getContractType() { return contractType; }
+}
