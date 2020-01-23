b6d9266598c9934eed4dbf5196945231259c7ebb
Sylvain Michel <sylmic@arvato-services.pl>
2020-01-23 16:43:40 +0100

    adding map with package but in comment - to fix

--- ContractFactory.java
+++ ContractFactory.java
@@ -2,9 +2,11 @@ package com.bartoszwalter.students.taxes;
 import Contract;
 import CivilContract;
 import OrdinaryContract;
+//import java.util.map;
 public class ContractFactory {
     public static Contract getContract(Double income, String ContractType){
-        /*Map<String, String> contractMap = new HashMap<String, String>();
+        /*
+        Map<String, String> contractMap = new HashMap<String, String>();
         contractMap.put("O", new OrdinaryContract(income));
         contractMap.put("C", new CivilContract(income));
         return contractMap
