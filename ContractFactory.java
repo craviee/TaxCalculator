a548055d4bb7084f91915cfd3587210afbdbf11c
Sylvain Michel <sylmic@arvato-services.pl>
2020-01-30 15:47:28 +0100

    Contract factory in map way

--- ContractFactory.java
+++ ContractFactory.java
@@ -5,21 +5,11 @@ import OrdinaryContract;
 //import java.util.map;
 public class ContractFactory {
     public static Contract getContract(Double income, String ContractType){
-        /*
+        
         Map<String, String> contractMap = new HashMap<String, String>();
         contractMap.put("O", new OrdinaryContract(income));
         contractMap.put("C", new CivilContract(income));
         return contractMap.get(ContractType);
-        */
-        switch(ContractType){
-            case "O":
-                return new OrdinaryContract(income);
-            case "C":
-                return new CivilContract(income);
-            default:
-                return null;
-        } 
-        
     }
 }
 
