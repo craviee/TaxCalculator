9a25154a73d8e2b934019d1a8ce7dd9cd89c35a5
Sylvain Michel <sylmic@arvato-services.pl>
2020-01-30 16:40:16 +0100

    adding partial OrdinaryContract.java

--- OrdinaryContract.java
+++ OrdinaryContract.java
@@ -1,5 +1,12 @@
 package com.bartoszwalter.students.taxes;
 import Contract;
 class OrdinaryContract extends Contract {
-
+    double constantIncomeCost=111.25;
+    double tax18=18/100;
+    public double taxBasis(){
+        return super.insurance().get("IncomeAfterInsurance")-constantIncomeCost;
+    }
+    public double advanceTax(){
+        return taxBasis() * tax18;
+    }
 }
