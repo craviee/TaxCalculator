8c5411b5f269528b7ba1e86a1bb860f4b6b222fb
Daniel Craviée <craviee@pm.me>
2020-01-30 15:46:21 +0100

    Revert "Feature/contract/dev"

--- Contract.java
+++ Contract.java
@@ -1,33 +1,14 @@
-package com.bartoszwalter.students.taxes;
-class Contract {
-    double rawIncome;
-    double IncomeAfterTax;
-    double percentPensionTax=9.76/100;
-    double percentDisableTax=1.5/100;
-    double percentIllnessTax=2.45/100;
-    double percent9HealthInsurance=9/100;
-    double percent775HealthInsurance=7.75/100;
- public Contract(double income){
-    rawIncome=income;
- }
- public Map tax(){
-    Map<String, Double> taxMap = new HashMap<String, Double>();
-    taxMap.put("Pension", rawIncome*percentPensionTax);
-    taxMap.put("Disability", rawIncome*percentDisableTax);
-    taxMap.put("Illness", rawIncome*percentIllnessTax);
-    taxMap.put("IncomeAfterTax", rawIncome-taxMap.get("Pension")-taxMap.get("Disability")-taxMap.get("Illness"));
-    return taxMap;
- }
- public Map insurance(){
-    Map<String, Double> insuranceMap = new HashMap<String, Double>();
-    insuranceMap.put("HealthAssurance9", IncomeAfterTax*percent9HealthInsurance);
-    insuranceMap.put("HealthAssurance775", IncomeAfterTax*percent775HealthInsurance);
-    insuranceMap.put("IncomeAfterInsurance", IncomeAfterTax-taxMap.get("HealthAssurance9")-taxMap.get("HealthAssurance775"));
-    return insuranceMap;
- }
-}
-/*with
+class with
 
+double RawIncome, Income
+and other variables that can be saved internally after calculations
+
+Also other some hardcoded variables
+
+double calculateIncome()
+double calculateGeneralTaxes()
+double calculateHealthTaxex
+...
 
 void printIncome()
 void printGeneralTaxes()
@@ -35,4 +16,4 @@ void printHeathTaxes()
 ...
 
 void performCalculations() -> could have the 3 calculations above and it is possible to add more
-void printData() -> could have the 3 prints above and it is possible to add more*/
+void printData() -> could have the 3 prints above and it is possible to add more
--- ContractFactory.java
+++ ContractFactory.java
@@ -1,26 +1,5 @@
-package com.bartoszwalter.students.taxes;
-import Contract;
-import CivilContract;
-import OrdinaryContract;
-//import java.util.map;
-public class ContractFactory {
-    public static Contract getContract(Double income, String ContractType){
-        /*
-        Map<String, String> contractMap = new HashMap<String, String>();
-        contractMap.put("O", new OrdinaryContract(income));
-        contractMap.put("C", new CivilContract(income));
-        return contractMap.get(ContractType);
-        */
-        switch(ContractType){
-            case "O":
-                return new OrdinaryContract(income);
-            case "C":
-                return new CivilContract(income);
-            default:
-                return null;
-        } 
-        
-    }
-}
+ContractFactory()
 
+getContract(RawIncome, Contract String)
+returns contract with the specified String or null if does not exist contract
 
